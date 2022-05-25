e0d2122ad51ca757365c4b21ccf9fb113cf7680f
Shawn Hellwege <shellwege2@unl.edu>
2022-05-25 16:12:58 -0500

    gulp file and package

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+};
+
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('buildStyles'));
+};
+
